ca195b42d3f82f7b1d38059e1e25759ee8cf5b4f
diegogj2024 <diegogarcia0809@outlook.com>
2025-05-21 16:57:39 -0500

    Add files via upload

--- proyecto/src/java/modelo/Enfermedades.java
+++ proyecto/src/java/modelo/Enfermedades.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author DIEGO
- */
+
 public class Enfermedades {
+    private String codigo_enfermedad;
+    private String corral;
+    private String Fecha_registro;
+    private String Humedad_del_terreno;
+    private String Nombre_enfermedad;
+    private String Tratamiento_aplicado;
+    private String observaciones;
+    
+    public Enfermedades() {
+        
+    }
+
+    public String getCodigo_enfermedad() {
+        return codigo_enfermedad;
+    }
+
+    public void setCodigo_enfermedad(String codigo_enfermedad) {
+        this.codigo_enfermedad = codigo_enfermedad;
+    }
+
+    public String getCorral() {
+        return corral;
+    }
+
+    public void setCorral(String corral) {
+        this.corral = corral;
+    }
+
+    public String getFecha_registro() {
+        return Fecha_registro;
+    }
+
+    public void setFecha_registro(String Fecha_registro) {
+        this.Fecha_registro = Fecha_registro;
+    }
+
+    public String getHumedad_del_terreno() {
+        return Humedad_del_terreno;
+    }
+
+    public void setHumedad_del_terreno(String Humedad_del_terreno) {
+        this.Humedad_del_terreno = Humedad_del_terreno;
+    }
+
+    public String getNombre_enfermedad() {
+        return Nombre_enfermedad;
+    }
+
+    public void setNombre_enfermedad(String Nombre_enfermedad) {
+        this.Nombre_enfermedad = Nombre_enfermedad;
+    }
+
+    public String getTratamiento_aplicado() {
+        return Tratamiento_aplicado;
+    }
+
+    public void setTratamiento_aplicado(String Tratamiento_aplicado) {
+        this.Tratamiento_aplicado = Tratamiento_aplicado;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    
     
 }
--- proyecto/src/java/modelo/Producto.java
+++ proyecto/src/java/modelo/Producto.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author DIEGO
- */
 public class Producto {
+    private String codigo_cultivo;
+    private String tipo_cultivo;
+    private String metodo_produccion;
+    private String frecuencia_produccion;
     
+    public Producto() {
+        
+    }
+
+    public String getCodigo_cultivo() {
+        return codigo_cultivo;
+    }
+
+    public void setCodigo_cultivo(String codigo_cultivo) {
+        this.codigo_cultivo = codigo_cultivo;
+    }
+
+    public String getTipo_cultivo() {
+        return tipo_cultivo;
+    }
+
+    public void setTipo_cultivo(String tipo_cultivo) {
+        this.tipo_cultivo = tipo_cultivo;
+    }
+
+    public String getMetodo_produccion() {
+        return metodo_produccion;
+    }
+
+    public void setMetodo_produccion(String metodo_produccion) {
+        this.metodo_produccion = metodo_produccion;
+    }
+
+    public String getFrecuencia_produccion() {
+        return frecuencia_produccion;
+    }
+
+    public void setFrecuencia_produccion(String frecuencia_produccion) {
+        this.frecuencia_produccion = frecuencia_produccion;
+    }
+    
+
 }
